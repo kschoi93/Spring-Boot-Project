0fec25196f82d6c708406d07d8f23feb437b8cee
kschoi <kyungsik0907@gmail.com>
2021-07-21 01:26:29 +0900

    codedeploy test 유료 크레딧으로 변경 후 재시도

--- src/test/java/hello/hellospring/service/BoardServiceTest.java
+++ src/test/java/hello/hellospring/service/BoardServiceTest.java
@@ -7,17 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import hello.hellospring.domain.entity.Board;
 import hello.hellospring.domain.repository.BoardRepository;
+import hello.hellospring.dto.BoardDto;
 
 public class BoardServiceTest {
     @Autowired
     private BoardRepository boardRepository;
 
     @Test
-    void 게시물_데이터_불러오기_확인() {
+    void 게시글_작성_테스트() {
         // given
-        List<Board> board = boardRepository.findAll();
+        BoardDto dto = BoardDto.builder().title("testcase").content("textcaseContent").author("test").build();
 
         // when
+        boardRepository.save(dto.toEntity());
 
         // then
 
