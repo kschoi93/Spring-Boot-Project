c79eafd2f2522b6095bd4be3de0909154936f630
kschoi <kyungsik0907@gmail.com>
2021-07-22 06:59:46 +0900

    travis 기타 설정 업데이트

--- src/main/java/hello/hellospring/service/BoardService.java
+++ src/main/java/hello/hellospring/service/BoardService.java
@@ -1,14 +1,10 @@
 package hello.hellospring.service;
 
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
-
-import org.aspectj.weaver.patterns.BindingAnnotationFieldTypePattern;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
